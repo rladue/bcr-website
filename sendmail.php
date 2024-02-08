cc8ed89902b82db85e8e3372365d47309eded97d
rladue <19434158+rladue@users.noreply.github.com>
2024-02-08 13:37:32 -0700

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,31 +1,26 @@
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if(
-        !empty($_POST['username'])
-        && !empty($_POST['message'])
-    ){
-        $username = $_POST["username"];
-        $server = $_POST["server"];
-        $issue = $_POST["issue"];
-        $message = $_POST["message"];
+   $username = $_POST["username"];
+    $server = $_POST["server"];
+    $issue = $_POST["issue"];
+    $message = $_POST["message"];
 
 
-        $to = "bluecollarrust@gmail.com";
-        $subject = "BlueCollarRust.com Report Submission";
-        $body = "Name: {$username}\n\nServer: {$server}\n\nIssue Type: {$issue}\n\nMessage: {$message}";
-        $headers = "From: {$username}";
+    $to = "bluecollarrust@gmail.com";
+    $subject = "BlueCollarRust.com Report Submission";
+    $body = "Name: {$username}\n\nServer: {$server}\n\nIssue Type: {$issue}\n\nMessage: {$message}";
+    $headers = "From: {$username}";
 
 
-        if (mail($to, $subject, $body, $headers)) {
-            echo '<script language="javascript">';
-            echo 'alert("Message successfully sent")';
-            echo '</script>';
-        } else {
-            echo '<script language="javascript">';
-            echo 'alert("Failed to send message")';
-            echo '</script>';
-        }
+    if (mail($to, $subject, $body, $headers)) {
+        echo '<script language="javascript">';
+        echo 'alert("Message successfully sent")';
+        echo '</script>';
+    } else {
+        echo '<script language="javascript">';
+        echo 'alert("Failed to send message")';
+        echo '</script>';
     }
 }
 ?>
