9dbb5390efc2bc90343b5efe842890fd1adc541a
rladue <19434158+rladue@users.noreply.github.com>
2024-02-08 13:07:11 -0700

    Adds report page
    - Finished report.hmtl
    - added sendmail.php

---
+++ sendmail.php
@@ -0,0 +1,26 @@
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(
+        !empty($_POST['username'])
+        && !empty($_POST['server'])
+        && !empty($_POST['issue'])
+        && !empty($_POST['message'])
+    ){
+        $username = $_POST["username"];
+        $server = $_POST["server"];
+        $issue = $_POST["issue"];
+        $message = $_POST["message"];
+
+
+        $to = "bluecollarrust@gmail.com";
+        $subject = "BlueCollarRust.com Report Submission";
+        $body = "Name: {$username}\n\nServer: {$server}\n\nIssue Type: {$issue}\n\nMessage: {$message}";
+        $headers = "From: {$username}";
+
+
+        if (mail($to, $subject, $body, $headers)) {
+            echo "Report sent successfully!";
+        } else {
+            echo "Failed to send message.";
+        }
+    }
+}
